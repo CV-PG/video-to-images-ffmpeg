d3507ec46c50619aa35758b864847de1f9fec86b
CV-PG <71630733+CV-PG@users.noreply.github.com>
2021-01-20 16:18:01 +0330

    Add files via upload

---
+++ run_ffmpeg.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 21 13:37:24 2018
+
+@author: RCISP
+"""
+
+import os
+
+##################----------Set Parameters----------####################
+video_input="video.mp4" # set video input
+output_format=".jpg" # setting image format
+start_time="00:00:00" # setting start time
+Duration="1:330:00" # setting Duration time
+Frame_per_second="2" # setting Frame Per Second
+
+########################################################################
+exe_code="ffmpeg.exe"
+path, name= os.path.split(video_input)
+name2=os.path.splitext(name)
+dirname=name2[0] 
+
+if not os.path.exists(path + '/' + dirname):
+    os.mkdir(path + '/' + dirname)
+else:
+    print('Directory:' + dirname + ' is already exist')
+   
+output_file=path + '/' + dirname
+os.system(exe_code + " -i " + video_input + " -ss " + start_time + " -t " + Duration + " -vf fps=" + Frame_per_second + " -q 0 " + output_file + "/img_%5d" + output_format)
+
